46f6ef027eb62fc780b17b91dd3d21c4423d4d81
Gu1ll0m <gzucchi.pro@gmail.com>
2018-01-30 11:31:19 +0100

    Update app.js

--- todo-list-project/V1/todo-list-app/js/app.js
+++ todo-list-project/V1/todo-list-app/js/app.js
@@ -1,72 +1,31 @@
-/*global NodeList */
-(function (window) {
+/*global app, $on */
+(function () {
 	'use strict';
 
 
 	/**
-	 * Récupére les éléments par le sélecteur CSS: qs = querySelector
+	 * Configure une toute nouvelle Todo list.
+	 * @param {string} (name) Le nom de votre nouvelle TODO list.
 	 */
-	window.qs = function (selector, scope) {
-		return (scope || document).querySelector(selector);
-	};
+	function Todo(name) {
+		this.storage = new app.Store(name); // this.storage = new app.Store avec en paramètre un nom
+		this.model = new app.Model(this.storage);
+		this.template = new app.Template();
+		this.view = new app.View(this.template);
+		this.controller = new app.Controller(this.model, this.view);
+	}
+
+	var todo = new Todo('todos-vanillajs'); // dans View.js, View.prototype.bind() et View.prototype.render()
 
 
 	/**
-	 * Récupére les éléments par le sélecteur CSS: qsa = querySelectorAll
+	 * Ajoute la route dans l' url ''|| active || completed
 	 */
-	window.qsa = function (selector, scope) {
-		return (scope || document).querySelectorAll(selector);
-	};
+	function setView() {
+		todo.controller.setView(document.location.hash);
+	}
 
 
-	/**
-	 * Encapsule l'addEventListener
-	 */
-	window.$on = function (target, type, callback, useCapture) {
-		target.addEventListener(type, callback, !!useCapture);
-	};
-
-
-	/**
-	 * Délègue un eventListener à un parent
-	 * @param  {[type]} (target)  La cible
-	 * @param  {[type]} (selector) Vérifie qu'il y a un match entre enfants et parents
-	 * @param  {[type]} (handler)  callback exécuté si il y a une certaine condition
-	 */
-	window.$delegate = function (target, selector, type, handler) {
-		function dispatchEvent(event) {
-			var targetElement = event.target; // cible l' élément
-			var potentialElements = window.qsa(selector, target); // qsa sur élément du même type
-			var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0; // est-ce que dans potentialElements il y a targetElement , si >= o il y a un index et ça match
-
-			if (hasMatch) {
-				handler.call(targetElement, event); // si on a un élément hasMatch on appel le gestionnaire sur l' élément cible
-			}
-		}
-		// https://developer.mozilla.org/en-US/docs/Web/Events/blur
-		var useCapture = type === 'blur' || type === 'focus'; // useCapture peut être de type blur ou focus
-		window.$on(target, type, dispatchEvent, useCapture); // $on ajoute un eventListener
-	};
-
-
-	/**
-	 * Recherchez le parent de l'élément avec le nom de tag : $parent(qs('a'), 'div');
-	 */
-	window.$parent = function (element, tagName) {
-		if (!element.parentNode) {
-			return; // si pas d' élément parent il ne se passe rien
-		}
-		if (element.parentNode.tagName.toLowerCase() === tagName.toLowerCase()) { // si le tagname de l' élément parent en minuscule est strictement égale à notre tagname
-			return element.parentNode; // on retourne notre élément parent
-		}
-		return window.$parent(element.parentNode, tagName);
-	};
-
-
-	/**
-	 * Autorisez les boucle sur les nœuds : qsa('.foo').forEach(function () {})
-	 */
-	NodeList.prototype.forEach = Array.prototype.forEach; // parcourir chaque noeuds revient à parcourir chaque tableau
-
-
-})(window);
+	$on(window, 'load', setView);
+	$on(window, 'hashchange', setView);
+})();
